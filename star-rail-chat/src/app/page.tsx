96f7e0f262d01e3f9fe060becf103a3081552169
Aaron Hong <46896330+AaronHongry@users.noreply.github.com>
2024-06-04 14:18:21 -0400

    Started Code

---
+++ star-rail-chat/src/app/page.tsx
@@ -0,0 +1,15 @@
+
+
+export default function Home() {
+  return (
+    <div style={{
+      backgroundImage: "url('/images/background.png')",
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center center',
+        height: '100vh',
+    }}>
+      <h1>TEST TEST TEST</h1>
+    </div>
+  );
+}
